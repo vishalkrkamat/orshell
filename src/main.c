78f89212443117b54b3f12f09382cdde8ecbced4
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-09-07 17:20:47 +0530

    minor changes in clang format and better exit for shell

--- src/main.c
+++ src/main.c
@@ -13,18 +13,14 @@ int main()
     size_t sizebuf = 16;
     char *buff     = malloc(sizebuf);
 
-    while (strcmp(buff, "exit") != 0)
-    {
-        buff                      = get_input(buff, sizebuf);
+    while (1) {
+        buff = get_input(buff, sizebuf);
+        if (strcmp(buff, "exit") == 0)
+            exit(0);
         buff[strcspn(buff, "\n")] = 0;
         char **arr                = tokenize(buff);
-        int size                  = 0;
-        for (; arr[size] != NULL; size++)
-        {
-        }
         create_process(arr);
-        for (int i = 0; arr[i] != NULL; i++)
-        {
+        for (int i = 0; arr[i] != NULL; i++) {
             free(arr[i]);
         }
         free(arr);
@@ -37,14 +33,11 @@ char *get_input(char *buff, size_t size)
     int c;
 
     printf("$ ");
-    while ((c = getchar()) != EOF && c != '\n')
-    {
-        if (len + 1 >= size)
-        {
+    while ((c = getchar()) != EOF && c != '\n') {
+        if (len + 1 >= size) {
             size      = 2 * size;
             char *tmp = realloc(buff, size);
-            if (!tmp)
-            {
+            if (!tmp) {
                 free(buff);
                 fprintf(stderr, "Failed allocation");
                 return NULL;
@@ -62,8 +55,7 @@ char **tokenize(char *input)
     char **arr = NULL;
     int size   = 0;
     char *tok  = strtok(input, " ");
-    while (tok != NULL)
-    {
+    while (tok != NULL) {
         arr = realloc(arr, (size + 1) * sizeof(char *));
 
         arr[size] = malloc(strlen(tok) + 1);
@@ -85,18 +77,13 @@ int create_process(char *argv[])
 
     pid_t pid = fork();
 
-    if (pid == 0)
-    {
+    if (pid == 0) {
         execvp(argv[0], argv);
         perror("execvp failed");
         _exit(1);
-    }
-    else if (pid > 0)
-    {
+    } else if (pid > 0) {
         wait(NULL);
-    }
-    else
-    {
+    } else {
         printf("fork failed\n");
         exit(1);
     }
